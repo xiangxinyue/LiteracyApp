8684c1a5b92d6dee52d11e0a061a4fd000195cce
kay7777 <kay7song@gmail.com>
2020-05-16 23:01:36 -0600

    remove

--- README.md
+++
@@ -1,9 +0,0 @@
-# Literacy Training Application -- Version 2.0
-
-## Language, Framework, and Libraries
-
-1. Language: JavaScript, HTML/CSS
-2. Frontend: React.js, Redux.js
-3. Backend: Node.js, Express.js, Passport.js, Mongoose.js
-4. Database: MongoDB, AWS S3
-5. Dev tools: Postman, Heroku, Google-OAuth2.0
--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    "620520764016-5i03p3dss97p815u8vmjc20sn6mg3n4q.apps.googleusercontent.com",
-  googleClientSecret: "485_dKqUJVSBK4YkS6zoQBpb",
-  mongoURI:
-    "mongodb+srv://kaysong:songqisyxx@cluster0-xs9l8.mongodb.net/LiteracyTrainLocal?retryWrites=true&w=majority",
-  cookieKey: "asndugfuwbe423423e21fnaidnuudhuewifiufanda",
-};
