9b38c68c0968b37cfa036d50f15c7de0e2648870
kay7777 <kay7song@gmail.com>
2020-05-16 23:08:33 -0600

    hide keys

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,7 @@
-module.exports = {
-  googleClientID:
-    "620520764016-5i03p3dss97p815u8vmjc20sn6mg3n4q.apps.googleusercontent.com",
-  googleClientSecret: "485_dKqUJVSBK4YkS6zoQBpb",
-  mongoURI:
-    "mongodb+srv://kaysong:songqisyxx@cluster0-xs9l8.mongodb.net/LiteracyTrainLocal?retryWrites=true&w=majority",
-  cookieKey: "asndugfuwbe423423e21fnaidnuudhuewifiufanda",
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else if (process.env.NODE_ENV === "ci") {
+  module.exports = require("./ci");
+} else {
+  module.exports = require("./dev");
+}
