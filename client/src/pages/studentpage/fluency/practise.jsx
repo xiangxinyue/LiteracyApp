2450a26c5f5b9f5babfdea0f71e181a39b0e70f8
kay7777 <kay7song@gmail.com>
2020-07-27 01:05:23 -0600

    add font choice

--- client/src/pages/studentpage/fluency/practise.jsx
+++ client/src/pages/studentpage/fluency/practise.jsx
@@ -2,22 +2,34 @@ import React from "react";
 import FluencyIntro from "../../../components/student/fluency/train/trainintro";
 import FluencyMain from "../../../components/student/fluency/train/trainpart";
 import FluencyHeader from "../../../components/student/fluency/assets/header";
+import FluencyFontSize from "../../../components/student/fluency/assets/fontSize";
 import Paper from "../../../assets/paper";
 import { Container } from "@material-ui/core";
 
 class FluencyPractise extends React.Component {
   state = {
     understand: false,
+    fontSizeChose: false,
+    fontSize: 4,
   };
 
   render() {
-    const { understand } = this.state;
+    const { understand, fontSizeChose } = this.state;
     return (
       <div>
         {understand ? (
-          <Container style={{ marginTop: "15%" }}>
-            <Paper component={FluencyMain} />
-          </Container>
+          fontSizeChose ? (
+            <Container style={{ marginTop: "15%" }}>
+              <FluencyMain fontSize={this.state.fontSize} />
+            </Container>
+          ) : (
+            <Container style={{ marginTop: "15%" }}>
+              <FluencyFontSize
+                newFont={(newFont) => this.setState({ fontSize: newFont })}
+                start={() => this.setState({ fontSizeChose: true })}
+              />
+            </Container>
+          )
         ) : (
           <div>
             <FluencyHeader part="Training Practise" />
