31770ba796066f09de6112835438fa5f1c73dbdb
kay7777 <kay7song@gmail.com>
2020-08-29 13:10:09 -0600

    config ttavis

--- tests/setup.js
+++ tests/setup.js
@@ -4,7 +4,7 @@ const keys = require("../config/keys");
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/literacy", {
+mongoose.connect(keys.mongoURI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
